695e922df9afa08d4d56b22857e99476bcab355c
vebradev <justinas@vebra.dev>
2019-12-17 12:24:43 +0200

    Button component formatting

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,35 +1,29 @@
-import React from "react";
-import styled from "styled-components";
-import * as Fonts from "./../variables/fonts";
+import React from 'react';
+import styled from 'styled-components';
+import * as Fonts from './../variables/fonts';
 
 const BTN = styled.button`
   font-family: ${Fonts.type.ROBOTO_MONO};
   font-weight: bold;
   font-size: 14px;
   text-align: center;
-  color: #ffffff;
+  color: #FFFFFF;
   height: 43px;
   padding: 12px 22px;
   border-radius: 6px;
   border: 0;
-  background-image: ${props => props.color};
-  background-color: #ffffff;
+  background-image: ${(props) => props.color};
+  background-color: #FFFFFF;
 
-  ${({ wide }) =>
-    wide &&
-    `
+  ${({ wide }) => wide && `
     width: 100%;
   `};
 
-  ${({ border }) =>
-    border &&
-    `
+  ${({ border }) => border && `
     border-color: ${border};
   `};
 
-  ${({ color }) =>
-    !color &&
-    `
+  ${({ color }) => !color && `
     color: #212121;
     border: 2px solid;
   `};
@@ -40,9 +34,7 @@ const BTN = styled.button`
 `;
 
 const Button = ({ children, color, wide, border }) => (
-  <BTN type="button" color={color} wide={wide} border={border}>
-    {children}
-  </BTN>
+  <BTN type="button" color={color} wide={wide} border={border}>{children}</BTN>
 );
 
 export default Button;
