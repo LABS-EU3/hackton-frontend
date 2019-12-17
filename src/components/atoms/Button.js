1977ddfd551b34565e3909100e6bcfa2d53c8075
vebradev <justinas@vebra.dev>
2019-12-17 11:30:29 +0200

    update Button component with option to take full width

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import * as Fonts from './../variables/fonts';
 
 const BTN = styled.button`
+  font-family: ${Fonts.type.ROBOTO_MONO};
+  font-weight: bold;
+  font-size: 14px;
+  text-align: center;
   color: #FFFFFF;
   height: 43px;
   padding: 12px 22px;
@@ -9,13 +14,17 @@ const BTN = styled.button`
   border: 0;
   background-image: ${(props) => props.color};
 
+  ${({ wide }) => wide && `
+    width: 100%;
+  `};
+
   &:hover {
     cursor: pointer;
   }
 `;
 
-const Button = ({ children, color, ...props }) => {
-  return <BTN type="button" color={color}>{children}</BTN>;
-};
+const Button = ({ children, color, wide }) => (
+  <BTN type="button" color={color} wide={wide}>{children}</BTN>
+);
 
 export default Button;
