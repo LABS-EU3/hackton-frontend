acc5c8ace822f878e0d9a29b75b5bee482b68cc6
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-12-18 13:20:37 +0000

    redirect to register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { GlobalStyles } from "./components/index";
 import SingupPage from "./components/views/SignupPage";
 import LoginPage from "./components/views/LoginPage";
@@ -11,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/register" component={SingupPage} />
         <Route exact path="/login" component={LoginPage} />
+        <Redirect to="/register" />
       </Switch>
     </>
   );
