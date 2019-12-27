1300610074039dc7fe1194121ba5b7f47a1632ed
PascalUlor <pascalulor@yahoo.com>
2019-12-27 04:44:00 +0100

    change display image for login page

--- src/components/views/LoginPage.jsx
+++ src/components/views/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../../assets/Signup.png";
+import image from "../../assets/Login.png";
 import { Unboarding } from "../templates";
 
 const LoginPage = () => (
