bcc07ac9f439c65e4923307faed0df7ceccbe7db
vebradev <justinas@vebra.dev>
2020-01-06 14:29:26 +0200

    General UI updates: change the heading font, increase paragraph margin

--- src/components/atoms/Paragraph.js
+++ src/components/atoms/Paragraph.js
@@ -1,18 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
 import { type, Solid } from '../index';
 
-const P = styled.p`
+const Paragraph = styled.p`
   font-family: ${type.ROBOTO};
   color: ${Solid.BLACK};
   font-size: 15px;
   font-weight: 400;
-  margin: 0 0 10px 0;
+  margin: 0 0 20px 0;
   padding: 0;
 `;
-
-const Paragraph = ({ children }) => {
-  return <P>{children}</P>;
-};
 
 export default Paragraph;
