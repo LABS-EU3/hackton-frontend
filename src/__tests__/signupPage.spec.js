67380e9549f4c96f51f1f28f4e8b0548f828c4a1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-06 10:41:34 +0000

    wip: [delivers signup test]

--- src/__tests__/signupPage.spec.js
+++ src/__tests__/signupPage.spec.js
@@ -2,3 +2,19 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Signup from '../components/views/SignupPage';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Signup />)
+})
+
+// describe('signup component', () => {
+//     test('provide button to toggle to closed and locked states', () => {
+//         expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument()
+//         expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument()
+//     })
+// })
+
