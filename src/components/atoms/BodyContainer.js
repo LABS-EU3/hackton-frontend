b507826961b3a07da5daaaaebe0deb4f65fe0f7e
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-01-06 14:33:18 +0300

    removed hero image in tablet and mobile view

--- src/components/atoms/BodyContainer.js
+++ src/components/atoms/BodyContainer.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { media } from "../index";
 
 const BodyContainer = styled.div`
   width: 1062px;
@@ -7,6 +8,27 @@ const BodyContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 60px 45px;
+
+  @media ${media.tablet} {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+
+    img {
+      display: none;
+    }
+  }
+
+  @media ${media.mobile} {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    img {
+      display: none;
+    }
+  }
 `;
 
 export default BodyContainer;
