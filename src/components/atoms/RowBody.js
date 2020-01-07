c5e8f1f4727f0c68714cded2eb42dc447f95a44f
vebradev <justinas@vebra.dev>
2020-01-07 14:53:46 +0200

    fix spacing & responsiveness issues

--- src/components/atoms/RowBody.js
+++ src/components/atoms/RowBody.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import {media} from "../index";
 
 export const RowBody = styled.div`
-  max-width: 700px;
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
