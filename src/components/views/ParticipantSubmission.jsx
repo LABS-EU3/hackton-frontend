9e077b343136f85bb082b804ff007ab64fa03943
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-17 09:39:54 +0000

    create Participant submission

---
+++ src/components/views/ParticipantSubmission.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import HackathonForm from "../templates/HackathonForm";
+
+const ParticipantSubmission = () => <HackathonForm />;
+
+export default ParticipantSubmission;
