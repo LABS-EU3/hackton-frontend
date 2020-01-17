7d41f9302e1d71cca7e75eed1e27ec2ce6c1df1a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-17 09:55:07 +0000

    render participant submission

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ import HackathonFormPage from "./components/views/HackathonFormPage";
 import HackathonSinglePage from "./components/views/HackathonSinglePage";
 import PrivateRoute from "./components/organisms/PrivateRoute";
 import EditHackathon from "./components/templates/EditHackathon";
+import ParticipantSubmission from "./components/views/ParticipantSubmission";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
+      {/* <GlobalStyles />
       <Switch>
         <Route exact path="/register" component={SignupPage} />
         <Route exact path="/login" component={LoginPage} />
@@ -24,6 +25,11 @@ function App() {
           exact
           path="/dashboard/new"
           component={HackathonFormPage}
+        />
+        <PrivateRoute
+          exact
+          path="/participant_submission"
+          component={ParticipantSubmission}
         />
         <PrivateRoute
           exact
@@ -37,7 +43,9 @@ function App() {
         />
         <Redirect to="/register" />
       </Switch>
-      <ToastContainer />
+      <ToastContainer /> */}
+
+      <ParticipantSubmission />
     </>
   );
 }
