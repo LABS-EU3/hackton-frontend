a9752a149935c34ed7cf80a0314853428395932a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-20 12:46:53 +0100

    create event

--- src/store/projectSubmission/actions.js
+++ src/store/projectSubmission/actions.js
@@ -2,3 +2,10 @@ export const ProjectSubmissionTypes = {
     CREATE_EVENT: "CREATE_EVENT"
 }
 
+export const createEvent = (eventData, history) => {
+    return {
+        type: ProjectSubmissionTypes.CREATE_EVENT,
+        payload: eventData,
+        history
+    };
+}
