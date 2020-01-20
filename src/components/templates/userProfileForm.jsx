9ae16234fab4dc4a11d35eb5b278f19669845a34
Ayomide Jones <ayormeday@gmail.com>
2020-01-20 23:21:07 +0100

    bug fixes

--- src/components/templates/userProfileForm.jsx
+++ src/components/templates/userProfileForm.jsx
@@ -13,19 +13,35 @@ import { H3 } from "../atoms/Heading";
 import { RowHead } from "../atoms/RowHead";
 import { RowBody } from "../atoms/RowBody";
 import { Column } from "../atoms/Column";
+import { Row } from "../atoms/Row";
 import { CardWide } from "../atoms/Card";
 import Label from "../atoms/Label";
 import Input from "../atoms/Input";
 import TextArea from "../atoms/TextArea";
 import Select from "../atoms/Select";
 import Button from "../atoms/Button";
+import profileImg from "../../assets/profile-image.png";
 
 const BodyContainerColumn = styled(BodyContainer)`
   flex-direction: column;
 `;
 const NewLabel = styled(Label)`
-padding-left: 3px;
+  padding-left: 3px;
 `;
+const CardWider = styled(CardWide)`
+  margin-left: 150px;
+`;
+const ProfileRow = styled(Row)`
+  justify-content: space-around;
+`;
+
+var border = {
+  borderRadius: "50%",
+  width: "20%"
+};
+var pad = {
+    marginRight: "5px",
+}
 
 const defaultState = {
   first_name: "",
@@ -55,15 +71,35 @@ const UserProfileForm = ({ initialState = defaultState }) => {
           </RowHead>
 
           <RowBody>
-            <CardWide>
+            <CardWider>
               <Formik
-                // onSubmit={handleSubmit}
                 initialValues={initialState}
                 validationSchema={schema}
                 enableReinitialize
               >
                 {({ errors, touched }) => (
                   <Form>
+                    <Column>
+                      <NewLabel htmlFor="image">Profile picture</NewLabel>
+                      <ProfileRow>
+                        <img
+                          alt="profile thumbnail"
+                          src={profileImg}
+                          style={border}
+                        />
+                        <Row>
+                          <Link>
+                            <Button to="/dashboard" color="blue" style={pad}>
+                              Upload New Picture
+                            </Button>
+                            <Button to="/dashboard" color="grey" anchor>
+                              Remove
+                            </Button>
+                          </Link>
+                        </Row>
+                      </ProfileRow>
+                    </Column>
+
                     <RowBody>
                       <Column>
                         <NewLabel htmlFor="first_name">First Name</NewLabel>
@@ -131,7 +167,7 @@ const UserProfileForm = ({ initialState = defaultState }) => {
                   </Form>
                 )}
               </Formik>
-            </CardWide>
+            </CardWider>
           </RowBody>
         </BodyContainerColumn>
       </WideBody>
