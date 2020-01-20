d2276d08f57853f37194509207784b14bc72a203
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-20 13:03:48 +0100

    wip

---
+++ src/store/projectSubmission/saga.js
@@ -0,0 +1,16 @@
+import { put, takeLatest, call, all, select } from "redux-saga/effects";
+import { toast } from "react-toastify";
+import { axiosWithAuth } from "../../utils/api";
+import { ProjectSubmissionTypes, createEvent } from "./actions";
+
+const userToken = state => state.currentUser.token;
+
+function createEventAsync({ payload, history }) {
+    try {
+        const token = yield select(userToken);
+        const { data } = yield axiosWithAuth(token).post(`/api/events/${id}/projects/submissions`, payload);
+        if (data) {
+            yield 
+        }
+    }
+}
