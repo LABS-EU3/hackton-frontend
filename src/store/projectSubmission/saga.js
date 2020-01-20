0a0f7e55da93f3957674338f37d62b18e55ab3a5
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-20 13:29:58 +0100

    create function to handle project submission

--- src/store/projectSubmission/saga.js
+++ src/store/projectSubmission/saga.js
@@ -1,16 +1,20 @@
 import { put, takeLatest, call, all, select } from "redux-saga/effects";
 import { toast } from "react-toastify";
 import { axiosWithAuth } from "../../utils/api";
-import { ProjectSubmissionTypes, createSubmission } from "./actions";
+import { ProjectSubmissionTypes, createSubmission, fetchAllSubmissions, submissionsError } from "./actions";
 
 const userToken = state => state.currentUser.token;
 
-function createEventAsync({ payload, history }) {
+function createProjectSubmissionAsync({ payload, history }) {
     try {
         const token = yield select(userToken);
-        const { data } = yield axiosWithAuth(token).post(`/api/events/${id}/projects/submissions`, payload);
+        const { data } = yield axiosWithAuth(token).post(`/api/events/${payload.event_id}/projects/submissions`, payload);
         if (data) {
-            yield 
+            yield put(fetchAllSubmissions(payload.event_id));
+            toast.success(`😀 ${data.message}`);
         }
+    } catch (error) {
+        yield put(submissionsError(error.message));
+        toast.error(`⚠️ ${error.message}`);
     }
 }
