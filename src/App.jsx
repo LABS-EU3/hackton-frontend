2d817474a2f6ed23e505fb39c1238a64bd32ebbd
Ayomide Jones <ayormeday@gmail.com>
2020-01-21 19:16:25 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import HackathonSinglePage from "./components/views/HackathonSinglePage";
 import PrivateRoute from "./components/organisms/PrivateRoute";
 import EditHackathon from "./components/templates/EditHackathon";
 import UserProfileFormPage from "./components/views/userProfileFormPage";
-import "react-toastify/dist/ReactToastify.css";
 import PageNotFound from "./components/views/PageNotFound";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -40,7 +40,7 @@ function App() {
         />
          <PrivateRoute
           exact
-          path="/dashboard/user/profile/edit"
+          path="/dashboard/profile/edit"
           component={UserProfileFormPage}
         />
         <Redirect to="/register" />
