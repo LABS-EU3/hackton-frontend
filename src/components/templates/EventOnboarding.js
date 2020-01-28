7b9b084ff02b91625e9456e6e2e26ffec20245ac
vebradev <justinas@vebra.dev>
2020-01-28 20:56:17 +0200

    add vertical nav with svg icons

--- src/components/templates/EventOnboarding.js
+++ src/components/templates/EventOnboarding.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import UserHeader from "../organisms/UserHeader";
+import VerticalNav from "../molecules/VerticalNav";
 import { Footer } from "../organisms/index";
 import WideBody from "../atoms/WideBody";
 import BodyContainer from "../atoms/BodyContainer";
@@ -27,6 +28,7 @@ const EventOnboarding = () => {
     <div>
       <UserHeader />
       <WideBody>
+        <VerticalNav />
         <BodyContainerColumn>
           <RowHead>
             <H3>My hackathons</H3>
