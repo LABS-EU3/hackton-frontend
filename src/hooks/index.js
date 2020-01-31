37960d2ca129c8458760fd61d0daf7a5f32b8ca2
Mike Perry Y Attara <mpyebattara@gmail.com>
2020-01-31 14:59:26 +0000

    cutom hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { axiosWithAuth } from '../utils/api';
-
+import { axiosWithAuth, selectToken } from '../utils/api';
 
 export const useUsers = () => {
   const [users, setUsers] = useState([]);
-  const { token } = useSelector(state => state.currentUser);
+  const token = useSelector(selectToken);
 
   useEffect(() => {
     const getUsers = async () => {
@@ -39,4 +38,44 @@ export const useSearchUserByEmail = () => {
   }, [searchString, users]);
 
   return [matches, searchString, setSearchString];
+}
+
+export const useParticipants = id => {
+  const [participants, setParticipants] = useState([]);
+  const token = useSelector(selectToken);
+  const fetchParticipants = async () => {
+    const {
+      data: { body }
+    } = await axiosWithAuth(token).get(`/api/events/${id}/participants`);
+    setParticipants(body);
+  };
+
+  useEffect(() => {
+    fetchParticipants();
+  });
+
+  return [participants, fetchParticipants];
+}
+
+export const useEventTeam = id => {
+  const [team, setTeam] = useState([]);
+  const token = useSelector(selectToken);
+  const fetchEventTeam = async () => {
+    const {
+      data: { body: { members } }
+    } = await axiosWithAuth(token).get(`/api/events/${id}/team`);
+    setTeam(members);
+  };
+
+  useEffect(() => {
+    fetchEventTeam();
+  })
+
+  return [team, fetchEventTeam];
+}
+
+export const useJudges = id => {
+  const team = useEventTeam(id);
+  const judges = team.filter(t => t.role_type === 'judge');
+  return judges;
 }
