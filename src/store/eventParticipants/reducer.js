a1f5e44b9fb3bc0bd79982d43eb7ce8df384e0ff
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-02-03 22:38:01 +0100

    wip

--- src/store/eventParticipants/reducer.js
+++ src/store/eventParticipants/reducer.js
@@ -1,9 +1,19 @@
 import { EventParticipantTypes } from "./actions";
 
 export const eventParticipantsReducer = (eventParticipants = [], action) => {
+  console.log('eventParticipants', eventParticipants)
   switch (action.type) {
+    case EventParticipantTypes.FETCH_ALL_PARTICIPANTS:
+      return {
+        ...eventParticipants
+      };
     case EventParticipantTypes.SET_EVENT_PARTICIPANTS:
       return action.payload;
+
+    case EventParticipantTypes.GET_USER_REGISTERED_EVENTS:
+      return {
+        ...eventParticipants
+      };
     default:
       return eventParticipants;
   }
