cc9568424421bbddd46d8bff26de3c78b750c25d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-02-03 11:33:30 +0100

    render user profile component

--- src/components/templates/userProfile.js
+++ src/components/templates/userProfile.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function userProfile () {
+export default function UserProfile () {
     return (
         <>
-            
+
         </>
     );
 }
--- src/components/views/UserProfileFormPage.jsx
+++ src/components/views/UserProfileFormPage.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UserProfileForm from "../templates/UserProfileForm";
-import { fetchUserProfile } from '../../store/user/actions';
+import { fetchUserProfile } from "../../store/user/actions";
+import UserProfile from "../../components/templates/UserProfile";
 
-
-const UserProfileFormPage = () =>{ 
-    const dispatch = useDispatch();
-    const { userId } = useSelector(state => state.currentUser);
-    const userDetails = useSelector(state => state.currentUser);
+const UserProfileFormPage = () => {
+  const dispatch = useDispatch();
+  const { userId } = useSelector(state => state.currentUser);
+  const userDetails = useSelector(state => state.currentUser);
   useEffect(() => {
     dispatch(fetchUserProfile(userId));
   }, [dispatch, userId]);
 
-  console.log('úser details', userDetails);
-return (
-<UserProfileForm initialState={userDetails}/>
-
-);
-}
+  console.log("user details", userDetails);
+  return (
+    <UserProfileForm initialState={userDetails} />
+  );
+};
 
 export default UserProfileFormPage;
