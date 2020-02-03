32833e9c0cdac56fdacc7c829413609712d1afc5
Mike Perry Y Attara <mpyebattara@gmail.com>
2020-02-03 22:46:04 +0000

    useAsync hook

---
+++ src/hooks/async.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export const useAsync = (request) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const req = async () => {
+      try {
+        setLoading(true);
+        const { data } = await request();
+        setData(data);
+      } catch ({ response }) {
+        setError(response);
+      } finally {
+        setLoading(false)
+      }
+    }
+    req();
+  }, [])
+
+  return [data, loading, error];
+}
