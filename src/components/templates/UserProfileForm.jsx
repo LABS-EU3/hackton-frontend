aad2917797031e2adce6b225c119a8a69d41a1a3
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-02-03 11:12:17 +0100

    redirect to dashboard after editing profile

--- src/components/templates/UserProfileForm.jsx
+++ src/components/templates/UserProfileForm.jsx
@@ -160,7 +160,7 @@ const UserProfileForm = ({initialState}) => {
                           Cancel
                         </Button>
                       </Link>
-                      <Button color="green" type="submit">
+                      <Button color="green" to="/dashboard" type="submit">
                         Save Changes
                       </Button>
                     </RowBody>
