22c2d37378afcdf8e26795e83069279d2daf4341
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-02-03 11:45:56 +0100

    add styles

--- src/components/templates/userProfile.js
+++ src/components/templates/userProfile.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import styled from "styled-components";
+import UserHeader from "../organisms/UserHeader";
+import { Footer } from "../organisms/index";
+import WideBody from "../atoms/WideBody";
+import Nav from "../molecules/Nav";
+import BodyContainer from "../atoms/BodyContainer";
+import { H3 } from "../atoms/Heading";
+import { RowHead } from "../atoms/RowHead";
+import { RowBody } from "../atoms/RowBody";
+import { Column } from "../atoms/Column";
+import { CardWide } from "../atoms/Card";
+import profileImg from "../../assets/profile-image.png";
+import ProfileImage from '../molecules/ProfileImage';
+
+const BodyContainerColumn = styled(BodyContainer)`
+  flex-direction: column;
+`;
 
 export default function UserProfile ({initialState}) {
     return (
