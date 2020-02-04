ba3ddedfb79bd35d68ecf8c25a81e03cee1f5737
vebradev <justinas@vebra.dev>
2020-02-04 12:44:43 +0200

    fix styling on forgot password view

--- src/components/views/resetPassword/ResetPassword.jsx
+++ src/components/views/resetPassword/ResetPassword.jsx
@@ -1,27 +1,22 @@
 import React from "react";
-import styled from "styled-components";
 import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useDispatch } from "react-redux";
 import Input from "../../atoms/Input";
 import WideBody from "../../atoms/WideBody";
 import BodyContainer from "../../atoms/BodyContainer";
-import { H3 } from "../../atoms/Heading";
-import { CardForm } from "../../atoms/Card";
-import { RowHead } from "../../atoms/RowHead";
 import { RowBody } from "../../atoms/RowBody";
-import { Column } from "../../atoms/Column";
 import { Footer } from "../../organisms/index";
 import Button from "../../atoms/Button";
 import { ErrorSpan } from "../../atoms/Span";
-import { forgotPassword } from '../../../store/user/actions';
-import Header from '../../organisms/Header';
+import { forgotPassword } from "../../../store/user/actions";
+import Header from "../../organisms/Header";
 import { useHistory } from "react-router-dom";
-
-const BodyContainerColumn = styled(BodyContainer)`
-  flex-direction: column;
-  justify-content: start;
-`;
+import HeroImage from "../../atoms/HeroImage";
+import Container from "../../atoms/Container";
+import { H1 } from "../../atoms/Heading";
+import { Paragraph } from "../../atoms/Paragraph";
+import image from "../../../assets/Password.png";
 
 const ResetPassword = () => {
   const dispatch = useDispatch();
@@ -42,43 +37,37 @@ const ResetPassword = () => {
     <div>
       <Header />
       <WideBody>
-        <BodyContainerColumn>
-          <RowHead>
-            <H3>Forgot Password</H3>
-          </RowHead>
-
-          <Column>
-            <CardForm>
-              <Formik
-                initialValues={{ email: "" }}
-                onSubmit={handleSubmit}
-                validationSchema={schema}
-              >
-                {({ errors, touched }) => (
-                  <Form>
-                    <Input
-                      display="wide"
-                      id="email"
-                      type="email"
-                      name="email"
-                    />
-                    {errors.name && touched.name ? (
-                      <div>{errors.name}</div>
-                    ) : null}
-                    <ErrorSpan>
-                      <ErrorMessage name="email" />
-                    </ErrorSpan>
-                    <RowBody>
-                      <Button type="submit" color="green">Submit</Button>
-                    </RowBody>
-                  </Form>
-                )}
-              </Formik>
-              Please Input Your Email and we will help you recover your
-              password.
-            </CardForm>
-          </Column>
-        </BodyContainerColumn>
+        <BodyContainer justify="center">
+          <HeroImage src={image} alt="Reset passowrd" />
+          <Container>
+            <H1>Reset the password</H1>
+            <Paragraph>
+              Enter your email address so we can reset your password and send a link to your inbox.
+            </Paragraph>
+            <Formik
+              initialValues={{ email: "" }}
+              onSubmit={handleSubmit}
+              validationSchema={schema}
+            >
+              {({ errors, touched }) => (
+                <Form>
+                  <Input display="wide" id="email" type="email" name="email" placeholder="Email address"/>
+                  {errors.name && touched.name ? (
+                    <div>{errors.name}</div>
+                  ) : null}
+                  <ErrorSpan>
+                    <ErrorMessage name="email" />
+                  </ErrorSpan>
+                  <RowBody>
+                    <Button size="wide" type="submit" color="blue">
+                      Reset Password
+                    </Button>
+                  </RowBody>
+                </Form>
+              )}
+            </Formik>
+          </Container>
+        </BodyContainer>
       </WideBody>
       <Footer />
     </div>
