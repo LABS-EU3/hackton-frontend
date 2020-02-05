7dcc1295c534a1bc4870b35fd3a01de4540d4bd8
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-02-05 15:42:30 +0000

    add loading

--- src/hooks/events.js
+++ src/hooks/events.js
@@ -1,6 +1,6 @@
-import { useState, useEffect, useCallback } from 'react';
-import { useSelector } from 'react-redux';
-import { axiosWithAuth, selectToken } from '../utils/api';
+import { useState, useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
+import { axiosWithAuth, selectToken } from "../utils/api";
 
 export const useParticipants = id => {
   const [participants, setParticipants] = useState([]);
@@ -13,15 +13,14 @@ export const useParticipants = id => {
       setParticipants(body);
     };
     fetchData();
-  }, [id, token])
+  }, [id, token]);
 
   useEffect(() => {
     fetchParticipants();
-
   }, [fetchParticipants]);
 
   return [participants, fetchParticipants];
-}
+};
 
 export const useEventTeam = id => {
   const [team, setTeam] = useState([]);
@@ -29,7 +28,9 @@ export const useEventTeam = id => {
   const fetchEventTeam = useCallback(() => {
     const fetchData = async () => {
       const {
-        data: { body: { members } }
+        data: {
+          body: { members }
+        }
       } = await axiosWithAuth(token).get(`/api/events/${id}/team`);
       setTeam(members);
     };
@@ -38,17 +39,16 @@ export const useEventTeam = id => {
 
   useEffect(() => {
     fetchEventTeam();
-
-  }, [fetchEventTeam])
+  }, [fetchEventTeam]);
 
   return [team, fetchEventTeam];
-}
+};
 
 export const useJudges = id => {
   const [team] = useEventTeam(id);
-  const judges = team.filter(t => t.role_type === 'judge');
+  const judges = team.filter(t => t.role_type === "judge");
   return judges;
-}
+};
 
 export const useTeams = id => {
   const [teams, setTeams] = useState([]);
@@ -56,22 +56,20 @@ export const useTeams = id => {
 
   const fetchTeams = useCallback(() => {
     const fetchData = async () => {
-      const { data: { body }
-      } = await axiosWithAuth(token).get(
-        `/api/events/${id}/participant-teams`
-      );
+      const {
+        data: { body }
+      } = await axiosWithAuth(token).get(`/api/events/${id}/participant-teams`);
       setTeams(body);
-    }
+    };
     fetchData();
   }, [id, token]);
 
   useEffect(() => {
     fetchTeams();
-
   }, [fetchTeams]);
 
   return [teams, fetchTeams];
-}
+};
 
 export const useCreatedTeam = (eventId, userId) => {
   const [teams, fetchTeams] = useTeams(eventId);
@@ -82,30 +80,39 @@ export const useCreatedTeam = (eventId, userId) => {
   }, [fetchTeams]);
 
   return team;
-}
+};
 
 export const useSubmissions = id => {
   const [submissions, setSubmissions] = useState([]);
   const token = useSelector(selectToken);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const fetchSubmissions = useCallback(() => {
     const fetchData = async () => {
-      const { data: { body }
-      } = await axiosWithAuth(token).get(
-        `/api/events/${id}/projects`
-      );
-      setSubmissions(body);
-    }
+      const req = async () => {
+        try {
+          setLoading(true);
+          const {
+            data: { body }
+          } = await axiosWithAuth(token).get(`/api/events/${id}/projects`);
+          setSubmissions(body);
+        } catch ({ response }) {
+          setError(response);
+        } finally {
+          setLoading(false);
+        }
+      };
+      req();
+    };
     fetchData();
   }, [id, token]);
 
   useEffect(() => {
     fetchSubmissions();
-
   }, [fetchSubmissions]);
 
-  return [submissions, fetchSubmissions];
-}
-
+  return [submissions, fetchSubmissions, loading, error];
+};
 
 export const useUserEvents = (perPage = 6, currentPage = 1) => {
   const token = useSelector(selectToken);
@@ -117,19 +124,21 @@ export const useUserEvents = (perPage = 6, currentPage = 1) => {
     const req = async () => {
       try {
         setLoading(true);
-        const { data } = await axiosWithAuth(token).get(`/api/events/user-events?perPage=${perPage}&currentPage=${currentPage}`);
+        const { data } = await axiosWithAuth(token).get(
+          `/api/events/user-events?perPage=${perPage}&currentPage=${currentPage}`
+        );
         setData(data);
       } catch ({ response }) {
         setError(response);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
     req();
   }, [token, perPage, currentPage]);
 
   return [data, loading, error];
-}
+};
 
 export const useRegisteredEvents = (perPage = 9, currentPage = 1) => {
   const token = useSelector(selectToken);
@@ -141,19 +150,21 @@ export const useRegisteredEvents = (perPage = 9, currentPage = 1) => {
     const req = async () => {
       try {
         setLoading(true);
-        const { data } = await axiosWithAuth(token).get(`/api/events/participants/user?perPage=${perPage}&currentPage=${currentPage}`);
+        const { data } = await axiosWithAuth(token).get(
+          `/api/events/participants/user?perPage=${perPage}&currentPage=${currentPage}`
+        );
         setData(data);
       } catch ({ response }) {
         setError(response);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
     req();
   }, [token, perPage, currentPage]);
 
   return [data, loading, error];
-}
+};
 
 export const useEvent = id => {
   const token = useSelector(selectToken);
@@ -170,11 +181,11 @@ export const useEvent = id => {
       } catch ({ response }) {
         setError(response);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
     req();
   }, [token, id]);
 
   return [data, loading, error];
-}
+};
