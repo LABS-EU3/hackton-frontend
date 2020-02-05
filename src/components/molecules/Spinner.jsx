c8a6b8fd721ccfa054634366ad0a6f550473318c
Mike Perry Y Attara <mpyebattara@gmail.com>
2020-02-05 12:41:59 +0000

    create Spinner

---
+++ src/components/molecules/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loader from 'react-loader-spinner';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+export default () => (
+  <Loader
+    type="Puff"
+    color="#00BFFF"
+    height={100}
+    width={100}
+    visible={true}
+  />
+);
